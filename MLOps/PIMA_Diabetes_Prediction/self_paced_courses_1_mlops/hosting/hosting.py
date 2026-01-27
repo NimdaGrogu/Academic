e42a3b1ea188e8e123b1f1f326bbc82c77196499
Jonathan Angeles <jonathan@nimda.sh>
2026-01-27 13:55:21 -0600

    Hosting fix

--- MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
+++ MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
@@ -2,13 +2,19 @@ from huggingface_hub import HfApi
 from dotenv import load_dotenv
 load_dotenv(dotenv_path="../.env")
 import os
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
+logger = logging.getLogger("hosting")
 
 HF_USERNAME = os.getenv("HF_USERNAME")
 HF_TOKEN = os.getenv("HF_TOKEN")
-
+logger.info("Deploying System ..")
 api = HfApi(token=HF_TOKEN)
 api.upload_folder(
-    folder_path="self_paced_courses_1_mlops/deployment",
+    folder_path="deployment",
     repo_id=f"{HF_USERNAME}/PIMA-Diabetes-Prediction",                                         # enter the Hugging Face username here
     repo_type="space",
     path_in_repo="",                          # optional: subfolder path inside the repo
