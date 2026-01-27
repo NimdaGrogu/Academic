eb37ae6d5810b57037e7f694bd925106d3f500e0
Jonathan Angeles <jonathan@nimda.sh>
2026-01-27 17:56:03 -0600

    workflow deploying

--- MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
+++ MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
@@ -20,7 +20,7 @@ repo_id = f"{HF_USERNAME}/PIMA-Diabetes-Prediction"
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="self_paced_courses_1_mlops/deployment",
+    folder_path="deployment",
     repo_id=f"{HF_USERNAME}/PIMA-Diabetes-Prediction",  # enter the Hugging Face username here
     repo_type = "space",
     path_in_repo = "",  # optional: subfolder path inside the repo
