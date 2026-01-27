6e3d6c82ffad7de786387ae9eda9dbe6c4411eb4
Jonathan Angeles <jonathan@nimda.sh>
2026-01-27 17:55:11 -0600

    workflow deploying

--- MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
+++ MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/hosting/hosting.py
@@ -16,30 +16,15 @@ logger.info("HF_TOKEN present: %s", "yes" if HF_TOKEN else "no")
 HF_USERNAME = os.getenv("HF_USERNAME")
 logger.info("HF_USERNAME present: %s", "yes" if HF_USERNAME else "no")
 repo_id = f"{HF_USERNAME}/PIMA-Diabetes-Prediction"
-repo_type = "space"
 
-try:
-    logger.info("Deploying System ..")
-    api = HfApi(token=HF_TOKEN)
-    # Step 1: Check if the space exists
-    api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    logger.info(f"Space '{repo_id}' Exist::Deploying")
-    repo_type="space"
-    # Step 2: Upload the files to deploy
-    logger.info(f"Uploading the data.")
-    api.upload_folder(
-        folder_path="/MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/deployment",
-        repo_id=repo_id,  # enter the Hugging Face username here
-        repo_type=repo_type,
-        path_in_repo="",  # optional: subfolder path inside the repo
-    )
-except RepositoryNotFoundError:
-    logger.info(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False, space_sdk="docker")
-    logger.info(f"Space '{repo_id}' created.")
-    api.upload_folder(
-        folder_path="MLOps/PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/deployment",
-        repo_id=repo_id,  # enter the Hugging Face username here
-        repo_type=repo_type,
-        path_in_repo="",  # optional: subfolder path inside the repo
-    )
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="self_paced_courses_1_mlops/deployment",
+    repo_id=f"{HF_USERNAME}/PIMA-Diabetes-Prediction",  # enter the Hugging Face username here
+    repo_type = "space",
+    path_in_repo = "",  # optional: subfolder path inside the repo
+)
+
+
+
